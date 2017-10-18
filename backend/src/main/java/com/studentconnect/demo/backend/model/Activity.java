1bda67cb973edf4d4eccc1690e56bec4e5b7b26f
Stephen Schools <stephen.schools>
2017-10-18 16:59:03 -0400

    updated models

--- backend/src/main/java/com/studentconnect/demo/backend/model/Activity.java
+++ backend/src/main/java/com/studentconnect/demo/backend/model/Activity.java
@@ -1,5 +1,10 @@
 package com.studentconnect.demo.backend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "activities")
 public class Activity {
     private int id;
     private String name;
--- backend/src/main/java/com/studentconnect/demo/backend/model/Club.java
+++ backend/src/main/java/com/studentconnect/demo/backend/model/Club.java
@@ -1,4 +1,10 @@
 package com.studentconnect.demo.backend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clubs")
 public class Club {
+
 }
