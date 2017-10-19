fe6cf6507f415d3054a034a91a8eb54991954a01
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-19 11:06:06 -0400

    added link for bootstrap

--- front-end/student-connect/src/components/App.js
+++ front-end/student-connect/src/components/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { Router, Route, Switch } from 'react-router';
 import logo from './logo.svg';
 import '../styles/App.css';
+import Login from "./StudentLogin"
 import RegistrationView from  '../containers/RegistrationView'
-import Login from "./Login"
+
 
 class App extends Component {
   render() {
     return (
     <div>
       <Login />
+
     </div>
     );
   }
--- front-end/student-connect/src/containers/RegistrationView.js
+++ front-end/student-connect/src/containers/RegistrationView.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import '../styles/registration.css';
 
-export default Class extends Component {
+class RegistrationView extends Component {
   render() {
     return(
-      <h3>Register your account here!</h3>
-      <form>
-        <fieldset>
-          <input placeholder="First Name" required/>
-          <input placeholder="Last Name" required/>
-          <input placeholder="Username" required/>
-          <input type="password" placeholder="Password" required/>
-          <input type="password" placeholder="Re-enter Password" required/>
-          <button type="Submit">Submit</button>
-        </fieldset>
-      </form>
+      <div>
+        <h3>Register your account here!</h3>
+        <form>
+          <fieldset>
+            <input placeholder="First Name" required/>
+            <input placeholder="Last Name" required/>
+            <input placeholder="Username" required/>
+            <input type="password" placeholder="Password" required/>
+            <input type="password" placeholder="Re-enter Password" required/>
+            <button type="Submit">Submit</button>
+          </fieldset>
+        </form>
+    </div>
     )
   }
 }
+
+export default RegistrationView;
