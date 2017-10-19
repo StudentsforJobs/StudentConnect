891aba32fb7afa7f0f99e1f37ec1a9b18236f5eb
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-19 11:06:50 -0400

    forgot to save so committing again

--- backend/src/main/java/com/studentconnect/demo/backend/model/Club.java
+++ backend/src/main/java/com/studentconnect/demo/backend/model/Club.java
@@ -1,4 +1,45 @@
 package com.studentconnect.demo.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clubs")
 public class Club {
+    private int id;
+    private String name;
+    private Student student;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Club() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
