9c3544930973d4b5cf68c7e608c7a55530673041
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-19 10:11:42 -0400

    added a bit more to the Registration View

--- front-end/student-connect/src/components/App.js
+++ front-end/student-connect/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import '../styles/App.css';
+import RegistrationView from  '../containers/RegistrationView'
 
 class App extends Component {
   render() {
--- front-end/student-connect/src/containers/RegistrationView.js
+++ front-end/student-connect/src/containers/RegistrationView.js
@@ -4,11 +4,14 @@ export default class extends Component {
   render() {
     return(
       <form>
-        <input placeholder="First Name"/>
-        <input placeholder="Last Name"/>
-        <input placeholder="Username"/>
-        <input placeholder="PassWord"/>
-        <input placeholder="Re-enter Password"/>
+        <fieldset>
+          <input placeholder="First Name"/>
+          <input placeholder="Last Name"/>
+          <input placeholder="Username"/>
+          <input type="password" placeholder="Password" required/>
+          <input type="password" placeholder="Re-enter Password" required/>
+          <button type="Submit">Submit</button>
+        </fieldset>
       </form>
     )
   }
