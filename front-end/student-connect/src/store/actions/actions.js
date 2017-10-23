ea4f467c4d3cad1d19e326f0269ed4091241673a
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-23 14:45:12 -0400

    making sure all is committed

---
+++ front-end/student-connect/src/store/actions/actions.js
@@ -0,0 +1,15 @@
+export const USER_LOGIN = 'USER_LOGIN';
+export const USER_REGISTRATION = 'USER_REGISTRATION'
+// export const UPDATE_USER = 'UPDATE_USER';
+
+// export const userLogin = username => {
+//   return {
+//     type: actionTypes.USER_LOGIN
+//   }
+// }
+//
+// export const userRegistration = register => {
+//   return {
+//     type: actionTypes.USER_REGISTRATION
+//   }
+// }
