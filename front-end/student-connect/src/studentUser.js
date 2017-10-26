753de25daf472286729980ef3c3a99452ab1e9cd
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-26 13:10:53 -0400

    made some changes to student users

--- front-end/student-connect/src/studentUser.js
+++ front-end/student-connect/src/studentUser.js
@@ -9,7 +9,32 @@ const studentUser =
     about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum iste quod, deserunt reiciendis laborum distinctio corrupti totam consequatur, dicta vero. Minima veritatis sequi vitae quisquam, ab exercitationem, blanditiis nemo sunt.",
     clubs: ["Chess", "Spanish"]
     activities: ["Football", "School Paper"]
-    isTeacher: false
+    isTeacher: false,
+    Subjects: ["English"]
+  }
+  {
+    id: '0',
+    firstName: "Sandy",
+    lastName: "Marcello",
+    userName: "smarc",
+    password: "halo73",
+    about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum iste quod, deserunt reiciendis laborum distinctio corrupti totam consequatur, dicta vero. Minima veritatis sequi vitae quisquam, ab exercitationem, blanditiis nemo sunt.",
+    clubs: ["Chess", "Spanish"]
+    activities: ["Football", "School Paper"]
+    isTeacher: false,
+    Subjects: ["English", "Math"]
+  }
+  {
+    id: '0',
+    firstName: "Janet",
+    lastName: "Wilson",
+    userName: "jwils",
+    password: "cars532",
+    about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum iste quod, deserunt reiciendis laborum distinctio corrupti totam consequatur, dicta vero. Minima veritatis sequi vitae quisquam, ab exercitationem, blanditiis nemo sunt.",
+    clubs: ["Chess", "Spanish"]
+    activities: ["Football", "School Paper"]
+    isTeacher: false,
+    Subjects: ["Math"]
   }
 ]
 export default studentUser;
