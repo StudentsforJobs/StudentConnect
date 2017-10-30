dc1b43c92139b253b23433988a9659a19d015f39
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-30 10:41:09 -0400

    added navbar options we can take out if we decide we dont want them

--- front-end/student-connect/src/components/NavBar.js
+++ front-end/student-connect/src/components/NavBar.js
@@ -6,6 +6,9 @@ export default class NavBar extends Component {
     return (
       <nav className="navbar navbar-toggleable-md navbar-inverse">
               <a className="navbar-brand title">Student Connect</a>
+              <a href="/logut" className="nav-options">Logout</a>
+              <a href="/profile" className="nav-options" >Profile</a>
+              <a href="/classmates" className="nav-options">Classmates</a>
               <img src="/studentconnect.png" width="30" height="30" className="d-inline-block align-top" alt="Student Connect"/>
             </nav>)
 }
