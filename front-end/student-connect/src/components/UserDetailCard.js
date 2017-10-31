7969095c6bc1083c3efe24831d65024dbe7560fd
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-31 15:11:09 -0400

    information is populating to the user detail card

--- front-end/student-connect/src/components/UserDetailCard.js
+++ front-end/student-connect/src/components/UserDetailCard.js
@@ -8,25 +8,32 @@ export default class UserDetailCard extends Component {
       id: '',
       firstName: '',
       lastName: '',
+      currentStudent: this._getStudent()
     }
   }
 
   componentDidMount() {
+    const that = this;
     axios.get(`http://localhost:8080/home`)
     .then(res => {
+      const student = this._getStudent()
       console.log(res);
+      that.setState({currentStudent: student})
     })
   }
-  // _getStudent(){
-  // return JSON.parse(localStorage.getItem('student'))
-  // }
+  _getStudent = () => {
+    const student = JSON.parse(localStorage.getItem('student'))
+    console.log(student)
+  return student
+}
 
 render(){
+
   return(
   <div className="profile-card" >
-    <img className="card-img-top profile-image" src="../student1.jpg" alt="StudentImg"/>
+    <img className="card-img-top profile-image" src={`../public/student${this.state.id}`} alt="StudentImg"/>
     <div className="card-block">
-      <h4 className="card-title">first name last name</h4>
+      <h4 className="card-title">{this.state.currentStudent.firstName} {this.state.currentStudent.lastName}</h4>
       <p className="card-text">Iron Yard High School</p>
       <a href="#" className="btn profile-btn">Go to profile</a>
     </div>
